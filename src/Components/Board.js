b0de99e29dcedb78482e51e8968acd2b5048b411
Artem Degtiar <artemdegtiar@MacBook-Pro-Artem.local>
2020-11-21 00:36:28 +0200

    initial commit

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,23 +2,47 @@ import React, { Component } from 'react'
 import Square from './Square'
 
 export default class Board extends Component {
+    constructor(props){
+
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+
+    }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+      }
+    
+   
+    renderSquare(id){
+      return(  
+        <Square 
+            value={this.state.squares[id]}  
+            onClick={() => this.handleClick(id)}
+        />
+    )}
+
+   
     render() {
         return (
             <div className="board">
                 <div className='row'>
-                    <Square id={1}/>
-                    <Square id={2}/>
-                    <Square id={3}/>
+                   {this.renderSquare(1)}
+                   {this.renderSquare(2)}
+                   {this.renderSquare(3)}
                 </div>
                 <div className='row'>
-                    <Square id={4}/>
-                    <Square id={5}/>
-                    <Square id={6}/>
+                   {this.renderSquare(4)}
+                   {this.renderSquare(5)}
+                   {this.renderSquare(6)}
                 </div>
                 <div className='row'>
-                    <Square id={7}/>
-                    <Square id={8}/>
-                    <Square id={9}/>
+                   {this.renderSquare(7)}
+                   {this.renderSquare(8)}
+                   {this.renderSquare(9)}
                 </div>
                
             </div>
